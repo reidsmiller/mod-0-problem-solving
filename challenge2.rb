41a3eb61c8b42a21f5a3298508157073ae4757bc
Reid Miller <reid.s.miller@gmail.com>
2023-01-06 11:01:11 -0700

    Complete challenge 2

---
+++ challenge2.rb
@@ -0,0 +1,7 @@
+# Use an array of strings with uppercase and lowercase letters that prints all the words in lowercase.
+# Create array, use .downcase to apply to every word and print.
+
+words = ["fiGure", "tOAd", "PantS", "BraCe", "noPE", "snOw", "puT"]
+
+words.downcase
+puts words
