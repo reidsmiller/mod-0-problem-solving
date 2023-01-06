1669bf2ca4065b9cdf93103572a496797e0915a4
Reid Miller <reid.s.miller@gmail.com>
2023-01-06 11:29:48 -0700

    Complete challenge 1

---
+++ challenge1.rb
@@ -0,0 +1,12 @@
+## Overall Goal: return only the strings that have exactly 4 charachers out of an array
+# Create an array with different chr lengths. Use array.each and shovel strings into four_chr_words array so it can be returned and not printed.
+
+#Final Solution
+words = ["figure", "toad", "pants", "brace", "nope", "snow", "put"]
+four_chr_words = []
+
+words.each do |word|
+    four_chr_words << word if word.length == 4
+end
+
+four_chr_words
