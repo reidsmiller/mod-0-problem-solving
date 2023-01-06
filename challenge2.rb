ed48fff5b756cd3c5fda9afe3ef09a1cc6bfd7a5
Reid Miller <reid.s.miller@gmail.com>
2023-01-06 12:30:25 -0700

    Complete challenge 3

--- challenge2.rb
+++ challenge2.rb
@@ -1,7 +1,9 @@
 # Use an array of strings with uppercase and lowercase letters that prints all the words in lowercase.
-# Create array, use .downcase to apply to every word and print.
+# Create array, use .each to select each element and use .downcase to apply to every word and print.
 
 words = ["fiGure", "tOAd", "PantS", "BraCe", "noPE", "snOw", "puT"]
 
-words.downcase
-puts words
+#Final Solution
+words.each do |word|
+    puts word.lowercase
+end
---
+++ challenge3.rb
@@ -0,0 +1,14 @@
+# Create an array of strings and **return** only words that begin with t.
+# Create array, use each to filter words using chr method, shovel into new array and return it.
+
+# Final Solution
+words = ["toggle", "follow", "grand", "trip", "rat"]
+t_words = []
+
+words.each do |word|
+    if word.start_with?("t")
+        t_words << word
+    end
+end
+
+t_words
