9d6780ec1d2a3a598827636f1b46d05b4b04294e
Reid Miller <reid.s.miller@gmail.com>
2023-01-06 12:39:49 -0700

    Complete challenge 4

--- challenge4.rb
+++ challenge4.rb
@@ -1,1 +1,8 @@
-# Overall Goal: 
+## Overall Goal: Create an array and print only words with "ing" in them.
+# Psuedocode: Create array. Add each method that checks for "ing" using include?
+
+words = ["talk", "pairing", "ingenious", "snowball", "running"]
+
+words.each do |word|
+    puts word if word.include?("ing")
+end
